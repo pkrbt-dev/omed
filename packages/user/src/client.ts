c0eda5eeb3d4c54e58b205cce7dae8ced6a5d33a
Anthonius Munthi <me@itstoni.com>
2025-08-03 06:41:35 +0800

    test(user): added user spec testing

--- packages/user/src/client.ts
+++ packages/user/src/client.ts
@@ -4,7 +4,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-console.log(process.env.OMED_DB_URL);
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export * from "../generated/client";
--- packages/user/tests/user.spec.ts
+++ packages/user/tests/user.spec.ts
@@ -1,5 +1,49 @@
+import { PrismaClient, User } from "../src/client";
+import { mock, mockDeep, mockReset } from "vitest-mock-extended";
+import createUser from "../src/user";
+
+const prisma = mockDeep<PrismaClient>();
+const MockUser = {
+  email: "email",
+  id: "id",
+  name: "name",
+};
 describe("User", () => {
-  it("should upsert user automatically", () => {
-    expect(true).toBeTruthy();
+  let user: ReturnType<typeof createUser>;
+
+  beforeEach(() => {
+    mockReset(prisma);
+    user = createUser(prisma);
+  });
+
+  it("should configures client", () => {
+    expect(user.client).toBeDefined();
+  });
+
+  it("should upsert user", async () => {
+    await user.upsert(MockUser);
+    expect(prisma.user.upsert).toBeCalledWith({
+      where: { email: MockUser.email },
+      create: MockUser,
+      update: MockUser,
+    });
+    expect(user).toBeDefined();
+  });
+
+  it("should remove user", async () => {
+    await user.remove("id");
+    const id = "id";
+
+    expect(prisma.user.delete).toBeCalledWith({ where: { id } });
+    await user.remove(id);
+  });
+
+  it("should find first user", async () => {
+    const email = "me@itstoni.com";
+    await user.findFirst({
+      email,
+    });
+
+    expect(prisma.user.findFirst).toBeCalledWith({ where: { email } });
   });
 });
